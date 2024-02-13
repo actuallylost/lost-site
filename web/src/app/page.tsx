66a621908f1ab110cd633863468faa86fc72d59e
Lost <pandagaminggr@gmail.com>
2024-02-13 19:23:56 +0200

    feat: add title; remove favicon

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,10 @@
 export const runtime = "edge";
 
 export default function Home() {
-	return <h1>Hello, world!</h1>;
+	return (
+		<>
+			<title>lost dev blog</title>
+			<h1>Hi, I'm lost and welcome to my dev blog! :D</h1>
+		</>
+	);
 }
