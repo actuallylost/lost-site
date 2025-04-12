ff8c2fbca059db328726ad1ffa08f7ecc621f9be
Lost <pandagaminggr@gmail.com>
2025-04-12 15:44:25 +0300

    chore: change title, remove description

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,8 +3,7 @@ import type { Metadata } from "next";
 export const runtime = "edge";
 
 export const metadata: Metadata = {
-	title: "actuallylost's portfolio",
-	description: "Hi I'm lost and this is my dev blog!",
+	title: "Lost's Developer Cave",
 };
 
 export default function RootLayout({
